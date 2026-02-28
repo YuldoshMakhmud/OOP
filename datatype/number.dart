4160c64d6f9c1727f2c04c9235105ea818167dbb
YuldoshMakhmud <yuldoshmakhmud1@gmail.com>
2026-02-28 23:42:18 +0900

    numbers

---
+++ datatype/number.dart
@@ -0,0 +1,12 @@
+// int
+// double
+// num
+void main() {
+  int a = 10;
+  double b = 3.14;
+  num c = 5; // num can hold both int and double
+
+  print('Integer: $a');
+  print('Double: $b');
+  print('Num: $c');
+}
